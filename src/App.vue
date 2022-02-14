ba17fd91087b6dce26cb2cd4d8c851ebd091e31e
Hitesh Pathar <hitesh@hulkapps.com>
2022-02-14 22:15:13 +0530

    New template

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Jewelry/>
+    <Contact/>
+    <Clients/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Clients from './components/Clients.vue'
+import Jewelry from './components/Jewelry.vue'
+import Contact from './components/Contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Clients,
+    Jewelry,
+    Contact,
+  }
+}
+</script>
+
+<style>
+  @import "assets/css/bootstrap.min.css";
+  /* @import "assets/css/bootsnav.css"; */
+  @import "assets/css/style.css";
+  /* @import "assets/css/owl.carousel.min.css"; */
+  /* @import "assets/css/owl.theme.default.min.css"; */
+  /* @import "assets/css/animate.css"; */
+  /* @import "assets/css/responsive.css"; */
+  /* @import "assets/css/linearicons.css"; */
+  /* @import "assets/css/font-awesome.min.css"; */
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg" style="display: none;">
+         <div class="loader"><img src="../assets/images/loading.gif" alt="#"></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header-top">
+            <div class="header">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                           <div class="center-desk">
+                              <div class="logo">
+                                 <a href="index.html"><img src="../assets/images/logo.png" alt="#"></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <div class="menu-area">
+                           <div class="limit-box">
+                              <nav class="main-menu" style="display: block;">
+                                 <ul class="menu-area-main">
+                                    <li class="active"> <a href="index.html">Home</a> </li>
+                                    <li> <a href="#jewellery">jewellery</a> </li>
+                                    <li> <a href="#contact">Contact</a> </li>
+                                    <li> <a href="#clients">Our Clients</a> </li>
+                                 </ul>
+                              </nav>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end header inner -->
+            <!-- end header -->
+            <section class="slider_section">
+               <div class="banner_main">
+                  <div class="container">
+                     <div class="row d_flex">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12 ">
+                           <div class="text-bg">
+                              <h1>Jewellery</h1>
+                              <span>Free Multipurpose Responsive <br> 
+                              <strong class="land_bold">Landing Page 2019</strong></span>
+                              <a href="#">Contact Us</a>
+                           </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                           <div class="text-img">
+                              <figure><img src="../assets/images/img.png"></figure>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+         
+         </section>
+         
+         </div>
+      </header>     
+      
+</div>
+</template>
+
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
